e715301362180633fbf9ecd6cd6fbc178de63fdd
micBar0ne <michelebarone1987@gmail.com>
2025-10-19 19:08:50 +0200

    Create SmaRI.ino

---
+++ SmaRI.ino
@@ -0,0 +1,13 @@
+// Arduino
+// Project name: SmaRI
+// Created by: micBar0ne
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
